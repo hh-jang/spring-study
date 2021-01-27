a9913e9173261b3cd947fa2174cc510f1c007224
hh-jang <jhh1117@daum.net>
2021-01-28 00:00:16 +0900

    ErrorHandlingAspect 추가
    * Exception 발생시 logging 하는 Aspect 추가

---
+++ src/main/java/com/hhjang/spring/study/aspect/ErrorHandlingAspect.java
@@ -0,0 +1,31 @@
+package com.hhjang.spring.study.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.ObjectError;
+
+@Aspect
+@Component
+@Slf4j
+public class ErrorHandlingAspect {
+
+    @Around("userServiceErrorHandling()")
+    public Object errorHandle(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object obj = null;
+        try {
+            obj = joinPoint.proceed();
+        } catch (Throwable e) {
+            log.error("occur error!!!!" + e.toString());
+            log.error("logging by aspect");
+        }
+
+        return obj;
+    }
+
+    @Pointcut("execution(* com.hhjang.spring.study.transaction.user.UserService*.*(..))")
+    public void userServiceErrorHandling() {}
+}
